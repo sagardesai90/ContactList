8147ef8624c5ff69a1baefd7284ec64d21583af7
sagardesai90 <saagardesai@gmail.com>
2019-12-17 14:34:52 -0800

    removed initial route

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ connectDB();
 //init middleware
 app.use(express.json({extended: false}));
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to the contact list API.'}));
-
 //define routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
