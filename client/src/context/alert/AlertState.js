5355f3c8b733b6d86bad2bda60fd2cd019659a7f
sagardesai90 <saagardesai@gmail.com>
2019-12-16 21:34:58 -0800

    user registration

---
+++ client/src/context/alert/AlertState.js
@@ -0,0 +1,37 @@
+import React, { useReducer } from 'react';
+import alertContext from './alertContext';
+import alertReducer from './alertReducer';
+import uuid from 'uuid';
+import {
+    SET_ALERT, REMOVE_ALERT
+} from '../types';
+
+const AlertState = props => {
+    const initialState = [];
+
+    const [state, dispatch] = useReducer(alertReducer, initialState);
+    //set aler
+    const setAlert = (msg, type, timeout = 5000) => {
+        const id = uuid.v4();
+        dispatch({
+            type: SET_ALERT,
+            payload: { msg, type, id }
+        });
+
+        setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id}), timeout)
+    }
+    //remove alert
+
+    
+    return (
+        <alertContext.Provider 
+        value={{
+            alerts: state,
+            setAlert
+        }}>
+            { props.children }
+        </alertContext.Provider>
+    )
+};
+
+export default AlertState;
